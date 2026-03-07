1696161349e2d348ff75dca746f32cf200f1b289
hlvini <hallen.vls@gmail.com>
2026-03-07 17:37:42 -0300

    Changed var names to English
    Added a timeout to avoid bottlenecks
    
    Changed how the 'next' button is found on page
    
    Browser starts in headless mode

--- orkut-crawler.py
+++ orkut-crawler.py
@@ -1,27 +1,33 @@
 import argparse
 import re
+import time
 from playwright.sync_api import sync_playwright
 
 # definicao dos argumentos necessarios para funcionamento do codigo
 parser = argparse.ArgumentParser()
 parser.add_argument("--index", required=True)
-parser.add_argument("--word", required=True)
+parser.add_argument("--term", required=True)
 args = parser.parse_args()
 
-url_inicial = f"https://web.archive.org/web/2/http://orkut.google.com/l-{args.letra}.html" # url do acervo 
+url_inicial = f"https://web.archive.org/web/2/http://orkut.google.com/l-{args.index}.html" # url do acervo 
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page() # abre aba
-    page.goto(url_inicial) # vai pro acervo
-    page.wait_for_load_state("networkidle") # espera carregar td
+
+    page.set_default_timeout(180000)
+
+    page.goto(url_inicial, wait_until="domcontentloaded")
 
     while True:
-        texto = page.inner_text("body")
-        if re.search(rf"\b{re.escape(args.palavra)}\b", texto, re.IGNORECASE): # garante que a palavra seja encontrada de forma inteira e não aglutinada, ignora case
-            print(f"WORD FOUND <<{args.palavra}>> IN -> ", page.url)
+        
+        print("SCANNED:", page.url)
 
-        next_btn = page.locator(f"text=next")
+        text = page.content()
+        if re.search(rf"\b{re.escape(args.term)}\b", text, re.IGNORECASE): # garante que a palavra seja encontrada de forma inteira e não aglutinada, ignora case
+            print(f"FOUND [{args.term}] IN ->", page.url)
+
+        next_btn = page.locator("a.paginationSeparator:has-text('next')")
         if next_btn.count() == 0:
             break
 
